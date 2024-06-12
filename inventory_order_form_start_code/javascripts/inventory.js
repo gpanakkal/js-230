659e1f15e859a7932d6b5ee48aa83ef35d9fe254
Gautam Panakkal <gautampanakkal@gmail.com>
2024-06-12 15:43:45 -0700

    finished refactoring

--- inventory_order_form_start_code/javascripts/inventory.js
+++ inventory_order_form_start_code/javascripts/inventory.js
@@ -1,91 +1,108 @@
-var inventory;
+const select = (selector) => document.querySelector(selector);
 
-(function() {
+class Inventory {
+  #lastId;
+  #collection;
 
-  inventory = {
-    lastId: 0,
-    collection: [],
-    setDate: function() {
-      var date = new Date();
-      $("#order_date").text(date.toUTCString());
-    },
-    cacheTemplate: function() {
-      var $iTmpl = $("#inventory_item").remove();
-      this.template = $iTmpl.html();
-    },
-    add: function() {
-      this.lastId++;
-      var item = {
-        id: this.lastId,
-        name: "",
-        stock_number: "",
-        quantity: 1
-      };
-      this.collection.push(item);
+  constructor(document) {
+    this.documentBody = document.body;
+    this.#lastId = 0;
+    this.#collection = [];
+    this.setDate();
+    this.cacheTemplate();
+    this.bindEvents();
+  }
 
-      return item;
-    },
-    remove: function(idx) {
-      this.collection = this.collection.filter(function(item) {
-        return item.id !== idx;
-      });
-    },
-    get: function(id) {
-      var found_item;
+  setDate() {
+    const date = new Date();
+    select('#order_date').textContent = date.toUTCString();
+  }
 
-      this.collection.forEach(function(item) {
-        if (item.id === id) {
-          found_item = item;
-          return false;
-        }
-      });
+  cacheTemplate() {
+    const source = select('#inventory_item');
+    const itemTemplate = Handlebars.compile(source.innerHTML);
+    this.template = itemTemplate;
+  }
 
-      return found_item;
-    },
-    update: function($item) {
-      var id = this.findID($item),
-          item = this.get(id);
+  bindEvents() {
+    select('#add_item').addEventListener('click', this.newItem.bind(this));
 
-      item.name = $item.find("[name^=item_name]").val();
-      item.stock_number = $item.find("[name^=item_stock_number]").val();
-      item.quantity = $item.find("[name^=item_quantity]").val();
-    },
-    newItem: function(e) {
-      e.preventDefault();
-      var item = this.add(),
-          $item = $(this.template.replace(/ID/g, item.id));
-      console.log({newItem: $item})
+    select('#inventory').addEventListener('click', (e) => {
+      if (e.target.tagName !== 'A') return;
+      return this.deleteItem.call(this, e);
+    });
 
-      $("#inventory").append($item);
-    },
-    findParent: function(e) {
-      return $(e.target).closest("tr");
-    },
-    findID: function($item) {
-      return +$item.find("input[type=hidden]").val();
-    },
-    deleteItem: function(e) {
-      e.preventDefault();
-      var $item = this.findParent(e).remove();
+    select('#inventory').addEventListener('focusout', (e) => {
+      const inputs = ['input', 'textarea', 'select', 'button'];
+      if (!inputs.includes(e.target.tagName.toLowerCase())) return;
+      return this.updateItem.call(this, e);
+    });
+  }
 
-      this.remove(this.findID($item));
-    },
-    updateItem: function(e) {
-      var $item = this.findParent(e);
+  add() {
+    this.#lastId += 1;
+    const item = {
+      id: this.#lastId,
+      name: '',
+      stock_number: '',
+      quantity: 1,
+    };
+    this.#collection.push(item);
+    return item;
+  }
 
-      this.update($item);
-    },
-    bindEvents: function() {
-      $("#add_item").on("click", $.proxy(this.newItem, this));
-      $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
-      $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
-    },
-    init: function() {
-      this.setDate();
-      this.cacheTemplate();
-      this.bindEvents();
+  remove(id) {
+    this.#collection = this.#collection.filter((item) => item.id !== id);
+  }
+
+  get(id) {
+    return this.#collection.find((item) => item.id === id) ?? false;
+  }
+
+  update(itemElement) {
+    const id = this.findID(itemElement);
+    const item = this.get(id);
+
+    item.name = document.querySelector('input[name^=item_name]').value;
+    item.stock_number = document.querySelector('input[name^=item_stock_number]').value;
+    item.quantity = document.querySelector('input[name^=item_quantity]').value;
+  }
+
+  newItem(e) {
+    e.preventDefault();
+    const item = this.add();
+    const itemElement = this.template({ itemId: item.id });
+    select('#inventory').insertAdjacentHTML('beforeend', itemElement);
+  }
+
+  findParent(e) {
+    const parentNodes = [];
+    let next = e.target;
+    while (true) {
+      if (!next) break;
+      parentNodes.push(next);
+      next = next.parentNode;
     }
-  };
-})();
+    return parentNodes.find((node) => node.tagName === 'TR');
+  }
 
-$($.proxy(inventory.init, inventory));
+  findID (itemElement) {
+    return Number(itemElement.querySelector('input[type=hidden]').value);
+  }
+
+  deleteItem(e) {
+    e.preventDefault();
+    const itemElement = this.findParent(e);
+    itemElement.remove();
+    this.remove(this.findID(itemElement));
+  }
+
+  updateItem(e) {
+    const itemElement = this.findParent(e);
+    this.update(itemElement);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const inventory = new Inventory(document);
+});
